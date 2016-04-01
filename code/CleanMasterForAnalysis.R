f8da2c4fd8a3f615d4ab2345a74354455897d23f
Laura Alexander <laurawalexander@gmail.com>
2016-04-01 15:39:15 -0700

    updating master file census data, and new code for analysis

---
+++ code/CleanMasterForAnalysis.R
@@ -0,0 +1,20 @@
+master_county_data <- read.csv("~/vortex/output/master_county_data/master_county_data.csv")
+View(master_county_data)
+
+
+countydata<- master_county_data[,c("state_fips", 
+                                   "county_fips", 
+                                   "CensusRace...STNAME",
+                                   "CensusRace...CTYNAME",
+                                   "land_area",
+                                   "CensusRace...TOT_POP",
+                                   "CensusRace...H",
+                                   "poverty_pct_2014...PCTPOVALL_2014",
+                                   "hail...total_intensity",
+                                   "tornado...total_intensity",
+                                   "wind...total_intensity", 
+                                   "natural_amenities...Scale"
+                                   )]
+countydata$highfirerisk<- master_county_data$Fire_risk_2012...risk_4+master_county_data$Fire_risk_2012...risk_5
+countydata$PercUnempl<- master_county_data$unemployed_2014...Unemployed_2014/master_county_data$CensusRace...TOT_POP
+countydata$PercMino<- (master_county_data$CensusRace...TOT_POP - master_county_data$CensusRace...WA)/master_county_data$CensusRace...TOT_POP
