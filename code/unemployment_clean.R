047aac955a85ba56d50ece8db603c9b16317deb0
Laura Alexander <laurawalexander@gmail.com>
2016-04-07 11:54:44 -0700

    Added cleaned poverty data, corrected issue with state FIPS inclusion in unemployment code

--- code/unemployment_clean.R
+++ code/unemployment_clean.R
@@ -3,13 +3,13 @@
 setwd("vortex")
 library(rio)
 
-pov.raw <- import("raw_data/poverty_unemployment/Unemployment.xls")
+unem.raw <- import("raw_data/poverty_unemployment/Unemployment.xls")
 
-pov <- pov.raw[-(1:(which(pov.raw[,1]=="FIPS_Code")-1)),] #remove the rows above the column names, held comments from original excel file
-colnames(pov) <- pov[which(pov[,1]=="FIPS_Code"),] #adds rownames
-if (which(pov$FIPS_Code=="FIPS_Code")!=0){pov <- pov[-which(pov$FIPS_Code=="FIPS_Code"),]} #if the names are still in a row, removes.
-pov <- pov[-is.na(pov$Rural_urban_continuum_code_2003),] #removes data for just states (leaves only counties)
-if (which(names(pov)=="FIPS_Code")!=0){names(pov)[which(names(pov)=="FIPS_Code")] <- "state_county_fips"} #changes FIPS code column
+unem <- unem.raw[-(1:(which(unem.raw[,1]=="FIPS_Code")-1)),] #remove the rows above the column names, held comments from original excel file
+colnames(unem) <- unem[which(unem[,1]=="FIPS_Code"),] #adds rownames
+if (which(unem$FIPS_Code=="FIPS_Code")!=0){unem <- unem[-which(unem$FIPS_Code=="FIPS_Code"),]} #if the names are still in a row, removes.
+unem <- unem[-which(is.na(unem$Rural_urban_continuum_code_2003)),] #removes data for just states (leaves only counties)
+if (which(names(unem)=="FIPS_Code")!=0){names(unem)[which(names(unem)=="FIPS_Code")] <- "state_county_fips"} #changes FIPS code column
 
 categories <- c(
   "state_county_fips",
@@ -18,7 +18,7 @@ categories <- c(
   "Median_Household_Income_2014"
 )
 
-pov.clean.2014 <- pov[,categories]
+unem.clean.2014 <- unem[,categories]
 
-write.csv(pov.clean.2014,"output/tidy_county_data/unemployed_2014.csv",row.names=F)
+write.csv(unem.clean.2014,"output/tidy_county_data/unemployed_2014.csv",row.names=F)
 
--- raw_data/census_other/poverty_clean.R
+++ raw_data/census_other/poverty_clean.R
@@ -3,7 +3,21 @@
 setwd("vortex")
 library(rio)
 
-pov <- import("C:/Users/Laura A/Downloads/PovertyEstimates.xls")
+pov.raw <- import("raw_data/poverty_unemployment/PovertyEstimates.xls")
 
-write.csv(pov,"C:/Users/Laura A/Downloads/pov_attempt.csv",row.names=F)
-  
+pov <- pov.raw[-(1:(which(pov.raw[,1]=="FIPStxt")-1)),] #remove the rows above the column names, held comments from original excel file
+colnames(pov) <- pov[which(pov[,1]=="FIPStxt"),] #adds rownames
+if (which(pov$FIPStxt=="FIPStxt")!=0){pov <- pov[-which(pov$FIPStxt=="FIPStxt"),]} #if the names are still in a row, removes.
+pov <- pov[-which(is.na(pov$Rural_urban_Continuum_Code_2003)),] #removes data for just states (leaves only counties)
+if (which(names(pov)=="FIPStxt")!=0){names(pov)[which(names(pov)=="FIPStxt")] <- "state_county_fips"} #changes FIPS code column
+
+categories <- c(
+  "state_county_fips",
+  "POVALL_2014",
+  "PCTPOVALL_2014"
+)
+
+pov.clean.2014 <- pov[,categories]
+
+write.csv(pov.clean.2014,"output/tidy_county_data/poverty_2014.csv",row.names=F)
+
